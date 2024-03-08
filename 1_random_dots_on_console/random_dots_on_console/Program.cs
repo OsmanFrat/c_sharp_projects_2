3b1c5bd5950f17cc94be0280a1ce66c458fe7940
OsmanFrat <69113898+OsmanFrat@users.noreply.github.com>
2024-03-08 19:39:25 +0300

    initial commit

---
+++ 1_random_dots_on_console/random_dots_on_console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace random_dots_on_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random yer = new Random();
+            Console.SetWindowSize(30, 20);
+            for (int i = 0; i < 100; i++)
+            {
+                int satir = yer.Next(1, 19);
+                int sutun = yer.Next(1, 20);
+                Console.SetCursorPosition(sutun, satir);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*");
+                System.Threading.Thread.Sleep(150);
+            }
+        }
+    }
+}
